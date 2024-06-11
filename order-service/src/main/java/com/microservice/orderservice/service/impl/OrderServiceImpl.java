b6b7ce3b733d7f520555c3b77f9638514b04b91b
Bijay Shrestha <7867129+bijay-shrestha@users.noreply.github.com>
2024-06-11 15:45:48 -0700

    Order service completed.

---
+++ order-service/src/main/java/com/microservice/orderservice/service/impl/OrderServiceImpl.java
@@ -0,0 +1,39 @@
+package com.microservice.orderservice.service.impl;
+
+import com.microservice.orderservice.dto.OrderLineItemsDTO;
+import com.microservice.orderservice.dto.OrderRequest;
+import com.microservice.orderservice.model.Order;
+import com.microservice.orderservice.model.OrderLineItems;
+import com.microservice.orderservice.repository.OrderRepository;
+import com.microservice.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest){
+        Order order  = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDTOList().stream()
+                .map(orderLineItemsDTO -> mapToDTO(orderLineItemsDTO, order)).toList();
+        order.setOrderLineItems(orderLineItems);
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDTO(OrderLineItemsDTO orderLineItemsDTO, Order order) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDTO.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDTO.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDTO.getSkuCode());
+        orderLineItems.setOrder(order);
+        return orderLineItems;
+    }
+}
